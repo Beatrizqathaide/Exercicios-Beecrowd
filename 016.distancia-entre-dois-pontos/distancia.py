1ef6fbc3ec85860d88ea2eb03546ca48dd21e8e6
BEATRIZ <beatrizzquaresma@gmail.com>
2022-09-06 20:16:37 -0400

    ajuste do math

--- 016.distancia-entre-dois-pontos/distancia.py
+++ 016.distancia-entre-dois-pontos/distancia.py
@@ -1,3 +1,4 @@
+
 #Leia os quatro valores correspondentes aos eixos x e y de dois pontosLeia os quatro valores correspondentes aos eixos x e y de dois pontos no plano, p1 (x1, y1) e p2 (x2, y2) e calcule a distância entre eles , mostrando quatro casas decimais após a vírgula, de acordo com a fórmula:
 
 from math import sqrt
@@ -10,6 +11,6 @@ x2, y2 = input("Valor: ").split(' ')
 x2 = float(x2)
 y2 = float(y2)
 
-distancia = math.sqrt(((x2 - x1) ** 2 + (y2 - y1) ** 2))
+distancia = sqrt(((x2 - x1) ** 2 + (y2 - y1) ** 2))
 
 print(f'{distancia:.4f}')
