02d667c24fae2b0172f89be287f12faff7ed77ec
BEATRIZ <beatrizzquaresma@gmail.com>
2022-10-02 21:03:43 -0400

    Atualização

--- Lista2/42.hora-do-evento/hora.py
+++ Lista2/42.hora-do-evento/hora.py
@@ -6,9 +6,29 @@ Você sabe que o evento pode levar de alguns segundos a alguns dias, portanto, v
 Obs: Considere que o evento do caso de teste tem a duração mínima de um minuto.
 '''
 
-diaInicio = int(input().split()[1]) #transforma em lista para pegar somente o número
-horaInicio, minutoInicio, segundoInicio = input().split(':')
+d1 = int(input().split()[1])
+h1, m1, s1 = map(int, input().split(':'))
+t1 = s1 + (m1 * 60) + (h1 * 60 * 60) + (d1 * 24 * 60 *60)
 
-diaFim = int(input().split()[1])
-horaFim, minutoFim, segundoFim = input().split(':')
+d2 = int(input().split()[1])
+h2, m2, s2 = map(int, input().split(':'))
 
+t2 = s2 + (m2 * 60) + (h2 * 60 * 60) + (d2 * 24 * 60 *60)
+
+diferenca = t2 - t1
+
+dia = diferenca // (24 * 60 * 60)
+diferenca = diferenca % (24 * 60 * 60)
+
+hora = diferenca // (60 * 60)
+diferenca = diferenca % (60 * 60)
+
+minuto = diferenca // 60
+diferenca = diferenca % 60
+
+segundo = diferenca
+
+print(f'{dia} dia(s)')
+print(f'{hora} hora(s)')
+print(f'{minuto} minuto(s)')
+print(f'{segundo} segundo(s)')
